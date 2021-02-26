efd887a49a6805f3e5c8ad16293630eb02c473b1
lucyy <lucy.xime11@hotmail.com>
2021-02-26 06:30:41 +0100

    Terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-	declarations: [ AppComponent ],
+	declarations: [ AppComponent],
 	imports: [ BrowserModule, AppRoutingModule ],
 	providers: [],
 	bootstrap: [ AppComponent ],
